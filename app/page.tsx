aa38fed2cc9ad1424526b42a4e715c12e3d8f70d
369webforall <369webforall@gmail.com>
2024-02-09 18:36:55 +0545

    fontend

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
-import Image from "next/image";
-
+import Slider from "@/components/Slider";
+import Featured from "@/components/Featured";
+import Offer from "@/components/Offer";
 export default function Home() {
   return (
-  <main><h1>This is home page</h1>
+  <main>
+    <Slider />
+    <Featured />
+    <Offer />
   </main>
   );
 }
