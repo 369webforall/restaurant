499e8be32884745c12fe204246afafc87ac7d492
369webforall <369webforall@gmail.com>
2024-02-11 11:49:52 +0545

    api

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,6 @@
-import NextAuth from "next-auth"
 import { authOptions } from "@/utils/auth";
+import NextAuth from "next-auth/next";
+
 const handler = NextAuth(authOptions)
 
-export { handler as GET, handler as POST }
+export {handler as GET, handler as POST}
--- app/api/categories/route.ts
+++ app/api/categories/route.ts
@@ -1,14 +1,17 @@
-import { NextRequest, NextResponse } from "next/server";
-import prisma from "@/prisma/client";
+import { prisma } from "@/utils/connect";
+import { NextResponse } from "next/server";
 
 
-export const GET = async(request:NextRequest)=>{
-    try {
-        const categories = await prisma.category.findMany();
-        return new NextResponse(JSON.stringify(categories), {status:500})
-
-    } catch (error) {
-        console.log(error)
-        return new NextResponse(JSON.stringify({message:"Something went wrong"}), {status:500})
-    }
-}
+// FETCH ALL CATEGORIES
+export const GET = async () => {
+  try {
+    const categories = await prisma.category.findMany();
+    return new NextResponse(JSON.stringify(categories), { status: 200 });
+  } catch (err) {
+    console.log(err);
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong!" }),
+      { status: 500 }
+    );
+  }
+};
